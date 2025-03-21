f96a7da67c5759c2860dd64fde2048033270ffaf
Zak-Riddles <90443917+Zak-Riddles@users.noreply.github.com>
2025-03-20 22:51:19 -0700

    File updated at 3/20/2025, 10:51:19 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,21 +1,30 @@
-// Last updated: 3/20/2025, 6:20:59 PM
+// Last updated: 3/20/2025, 10:51:19 PM
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) < 2:   # already filtered
-            return len(nums)
-       
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
         
-        curr_num = nums[0]
-        i = 1
-        safe_i = 0  # safe index to replace
-        while i < len(nums):
-            if nums[i] <= curr_num:
-                nums[safe_i] = nums[i]
-                if curr_num == nums[-1]:    # all uniques found
-                    break
-                i += 1
+        table = {}
+        for row in range(numRows):
+            table[row] = []
+        row = -1
+        is_down = True
+
+        for char in s:
+            if (row <= 0):
+                is_down = True
+            elif (row == numRows - 1):
+                is_down = False
+            
+            if is_down:
+                row += 1
+                table[row].append(char)
             else:
-                curr_num = nums[i]
-                safe_i += 1
-                
-        return safe_i + 1
+                row -= 1
+                table[row].append(char)
+        
+        result = []
+        for row_num in range(numRows):
+            result.append("".join(table[row_num]))
+        
+        return "".join(result)

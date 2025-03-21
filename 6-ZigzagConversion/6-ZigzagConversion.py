9fb393ff71a6400f99515ca11a81a7da7f0f9b36
Zak-Riddles <90443917+Zak-Riddles@users.noreply.github.com>
2025-03-20 23:03:16 -0700

    Create file

---
+++ 6-ZigzagConversion/6-ZigzagConversion.py
@@ -0,0 +1,30 @@
+// Last updated: 3/20/2025, 11:03:16 PM
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        
+        table = {}
+        for row in range(numRows):
+            table[row] = []
+        row = -1
+        is_down = True
+
+        for char in s:
+            if (row <= 0):
+                is_down = True
+            elif (row == numRows - 1):
+                is_down = False
+            
+            if is_down:
+                row += 1
+                table[row].append(char)
+            else:
+                row -= 1
+                table[row].append(char)
+        
+        result = []
+        for row_num in range(numRows):
+            result.append("".join(table[row_num]))
+        
+        return "".join(result)
